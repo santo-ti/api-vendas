89c50b009b89c510d6a20ebf38205133f48d7a4b
Anderson Santo <kalangoti@gmail.com>
2022-03-06 20:38:39 -0300

    added Static route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,6 +6,7 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import { errors } from 'celebrate';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(errors());
 
