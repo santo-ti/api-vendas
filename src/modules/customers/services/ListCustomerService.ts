3d4c13bc7efd742951be2bfc2834186d178deb99
Anderson Santo <kalangoti@gmail.com>
2022-12-07 21:38:04 -0300

    feat: Add Pagination

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,11 +1,24 @@
-import CustomerRepository from '@repositories/CustomerRepository';
 import { Customer } from '@entities/Customer';
 import { getCustomRepository } from 'typeorm';
+import CustomerRepository from '@repositories/CustomerRepository';
+
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
 
 export default class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
+  public async execute(): Promise<IPaginateCustomer> {
     const repository = getCustomRepository(CustomerRepository);
 
-    return await repository.find();
+    const entity = await repository.createQueryBuilder().paginate();
+
+    return entity as IPaginateCustomer;
   }
 }
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { pagination } from 'typeorm-pagination';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(pagination);
 
 app.use(routes);
 app.use('/files', express.static(uploadConfig.directory));
