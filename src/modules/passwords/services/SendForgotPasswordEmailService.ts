bcd950778f45bdef2742e515b6b13afe53e14eb3
Anderson Santo <kalangoti@gmail.com>
2023-01-07 20:56:03 -0300

    feat: Add Config Amazon SES - E-mail

--- src/modules/passwords/services/SendForgotPasswordEmailService.ts
+++ src/modules/passwords/services/SendForgotPasswordEmailService.ts
@@ -4,6 +4,8 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import EtherealMail from '@config/mail/EtherealMail';
 import path from 'path';
+import SESMail from '@config/mail/SESMail';
+import mailConfig from '@config/mail';
 
 interface IRequest {
   email: string;
@@ -23,6 +25,24 @@ export default class SendForgotPasswordEmailService {
     const { token } = await userTokenRepository.generate(user.id);
 
     const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
+
+    if (mailConfig.driver === 'ses') {
+      await SESMail.sendMail({
+        to: {
+          name: user.name,
+          email: user.email,
+        },
+        subject: '[API Vendas] Recuperação de Senha',
+        templateData: {
+          file: forgotPasswordTemplate,
+          variables: {
+            name: user.name,
+            link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+          },
+        },
+      });
+      return;
+    }
 
     await EtherealMail.sendMail({
       to: {
