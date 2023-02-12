f3973921efd25f8808c201593480faa9e46cd0a2
Anderson Santo <kalangoti@gmail.com>
2023-02-12 20:45:59 -0300

    feat: Build com o Babel

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: { node: 'current' },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config': './src/config',
+          '@modules': './src/modules',
+          '@entities': './src/shared/typeorm/entities',
+          '@repositories': './src/shared/typeorm/repositories',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
